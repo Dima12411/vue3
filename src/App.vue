773598024786502d2b3d87df9c52d405a719537b
Dima12411 <fixer40delta@gmail.com>
2022-08-04 18:30:47 +0300

    posts

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div>
-    <button @click="addLike">Like</button>
-    <button @click="addDislike">Dislike</button>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
   </div>
-  <div>Number of likes: <strong>{{ likes }}</strong></div>
-  <div>Number of dislikes: <strong>{{ dislikes }}</strong></div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 2, title: 'JavaScript 3', body: 'Описание поста 3'},
+      ]
     }
   },
   methods: {
-    addLike() {
-      this.likes += 1
-    },
-    addDislike() {
-      this.dislikes += 1
-    }
+
   }
 }
 
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
 </style>

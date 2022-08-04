d808c4efff7f3b79a2cd6b09665928eb7035e911
Dima12411 <fixer40delta@gmail.com>
2022-08-04 14:29:12 +0300

    rendering the number likes and dislikes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button>Like</button>
+    <button>Dislike</button>
+  </div>
+  <div>Number of likes: <strong>{{ likes }}</strong></div>
+  <div>Number of dislikes: <strong>{{ dislikes }}</strong></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 5,
+      dislikes: 0,
+    }
+  },
+}
+
+</script>
+
+<style>
+
+</style>

e9ed3fa6a55855c873a78464cdffefb323279087
Dima12411 <fixer40delta@gmail.com>
2022-09-14 12:47:48 +0300

    routing

---
+++ src/pages/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+    <h1 class="title">Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  font-size: 40px;
+}
+
+.rout {
+  text-decoration: none;
+  color: teal;
+  font-size: 20px;
+}
+
+.rout:hover {
+  opacity: 0.5;
+}
+
+.rout:active {
+  color: black;
+}
+</style>
